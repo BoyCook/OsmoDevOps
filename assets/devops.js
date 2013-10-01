104602827764cf3d67f6a130734eb82f446d3280
Craig Cook <boycook@me.com>
2013-10-01 17:08:12 +0100

    Got basic project rendering in place

--- assets/devops.js
+++ assets/devops.js
@@ -6,6 +6,8 @@ function DevOps() {
 		github: 'https://api.github.com',
 		travis: 'https://api.travis-ci.org'
 	}
+	this.templates = {};
+	this.loadTemplates();
 	// $.getJSON('https://api.travis-ci.org/repos?owner_name=BoyCook')
 	// $.getJSON('https://api.github.com/users/BoyCook/repos?per_page=100')	
 }
@@ -14,18 +16,31 @@ DevOps.prototype.loadRepos = function() {
 	for (var i=0,len=this.accounts.length; i<len; i++) {
 		this.getRepos(this.accounts[i]);
 	}
-}
+};
 
 DevOps.prototype.getRepos = function(name) {
 	var context = this;
 	var url = this.urls.github + '/users/' + name +  '/repos?per_page=100';
 	var callBack = function(data) {
-		context.repos[name] = data;
+		var repos = data;
+		context.repos[name] = repos;
+		$('.content').append(context.templates.list(repos));
 	};
 	$.getJSON(url, callBack);
 };
 
-var app = new DevOps();
+DevOps.prototype.loadTemplates = function() {
+	this.templates = {
+		list: Handlebars.compile($("#project-list-template").html())
+	};	
+};
+
+var app = undefined;
+
+$(document).ready(function(){
+	app = new DevOps();
+	app.loadRepos();
+});
 
 if (!(typeof exports === "undefined")) {
     exports.DevOps = DevOps;

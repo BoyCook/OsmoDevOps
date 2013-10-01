1a6a4384e28ca4c659a3e8b5c3f6468600b9b5ae
Craig Cook <boycook@me.com>
2013-10-01 16:37:42 +0100

    Got basic get repos working

--- assets/devops.js
+++ assets/devops.js
@@ -1,28 +1,32 @@
 
 function DevOps() {
-
+	this.accounts = ['tiddlyweb', 'TiddlySpace'];
+	this.repos = {};
+	this.urls = {
+		github: 'https://api.github.com',
+		travis: 'https://api.travis-ci.org'
+	}
+	// $.getJSON('https://api.travis-ci.org/repos?owner_name=BoyCook')
+	// $.getJSON('https://api.github.com/users/BoyCook/repos?per_page=100')	
 }
 
+DevOps.prototype.loadRepos = function() {
+	for (var i=0,len=this.accounts.length; i<len; i++) {
+		this.getRepos(this.accounts[i]);
+	}
+}
+
+DevOps.prototype.getRepos = function(name) {
+	var context = this;
+	var url = this.urls.github + '/users/' + name +  '/repos?per_page=100';
+	var callBack = function(data) {
+		context.repos[name] = data;
+	};
+	$.getJSON(url, callBack);
+};
+
+var app = new DevOps();
 
 if (!(typeof exports === "undefined")) {
-    exports.SPA = SPA;
+    exports.DevOps = DevOps;
 }
-
-
-// <table>
-//    <thead>
-//        <tr>    
-//           <th>Project</th><th>Status</th>
-//       </tr>
-//    </thead>
-//    <tbody>
-//       <tr>
-//          <td>TiddlyWeb</td>
-//          <td><a href="https://travis-ci.org/tiddlyweb/tiddlyweb"><img alt="Build Status" src="https://travis-ci.org/tiddlyweb/tiddlyweb.png?branch=master"></a></td>
-//       </tr> 
-//       <tr>
-//          <td>TiddlySpace</td>
-//          <td><a href="https://travis-ci.org/tiddlyweb/tiddlyweb"><img alt="Build Status" src="https://travis-ci.org/tiddlyweb/tiddlyweb.png?branch=master"></a></td>
-//       </tr> 
-//    </tbody>
-// </table>

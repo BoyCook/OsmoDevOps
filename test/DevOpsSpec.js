911c0b9fa9e50636c69df49cde2d20a539619267
Craig Cook <boycook@me.com>
2013-10-03 16:00:21 +0100

    Added tests

---
+++ test/DevOpsSpec.js
@@ -0,0 +1,14 @@
+var should = require('should');
+var DevOps = require('../assets/devops.js').DevOps;
+
+describe('DevOps', function () {
+    before(function() {
+
+    });
+
+    describe('#DevOps', function () {
+        it('should work', function () {
+
+        });
+    });
+});

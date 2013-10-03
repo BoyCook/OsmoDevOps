0c7afc51c2dd0b94c12e564defbffe4433cecc0d
Craig Cook <boycook@me.com>
2013-10-03 22:58:53 +0100

    Started space inclusion

--- assets/devops.js
+++ assets/devops.js
@@ -8,9 +8,21 @@ function DevOps() {
 	}
 	this.templates = {};
 	this.loadTemplates();
+	this.currentUser = tiddlyweb.status.username;
+    this.baseURL = "http://" + window.location.hostname + ':' + window.location.port;
+    this.space = new Space(this.baseURL, this.spaceName, this);
 	// $.getJSON('https://api.travis-ci.org/repos?owner_name=BoyCook')
 	// $.getJSON('https://api.github.com/users/BoyCook/repos?per_page=100')	
 }
+
+DevOps.prototype.getConfig = function() {
+    // context.space.fetchTiddler({ title: 'SiteSubtitle', bag:  context.spaceName + '_public'}, 
+    //     function(subTitleTiddler){
+    //         title += ' - ' + subTitleTiddler.text;
+    //         $('title').text(title);
+    //         $('header h1').text(title);                
+    // }, context.silentError);                    	
+};
 
 DevOps.prototype.loadRepos = function() {
 	for (var i=0,len=this.accounts.length; i<len; i++) {
@@ -30,15 +42,15 @@ DevOps.prototype.getRepos = function(name) {
 
 DevOps.prototype.loadTemplates = function() {
 	this.templates = {
-		list: Handlebars.compile($("#project-list-template").html())
+		list: Handlebars.compile($('#project-list-template').html())
 	};	
 };
 
-var app = undefined;
+var devOps = undefined;
 
 $(document).ready(function(){
-	app = new DevOps();
-	app.loadRepos();
+	devOps = new DevOps();
+	devOps.loadRepos();
 });
 
 if (!(typeof exports === "undefined")) {

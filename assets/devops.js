491bf753b27f3b750587f98bb2c8c7bb14ece842
Craig Cook <boycook@me.com>
2013-10-03 12:04:50 +0100

    Better github account definition - supporting explicit repo inclusion

--- assets/devops.js
+++ assets/devops.js
@@ -1,29 +1,56 @@
 
 function DevOps() {
-	this.accounts = ['tiddlyweb', 'TiddlySpace', 'TiddlyWiki'];
-	this.repos = {};
+	this.config.accounts = {
+		tiddlyweb: '*', 
+		TiddlySpace: '*', 
+		TiddlyWiki: '*',
+		BoyCook: ['SpaceUI', 'OsmoDevOps']
+	};
+	this.accounts = {};
 	this.urls = {
 		github: 'https://api.github.com',
 		travis: 'https://api.travis-ci.org'
 	}
 	this.templates = {};
 	this.loadTemplates();
-	// $.getJSON('https://api.travis-ci.org/repos?owner_name=BoyCook')
-	// $.getJSON('https://api.github.com/users/BoyCook/repos?per_page=100')	
+	this.setupCache();
+	// 'https://api.travis-ci.org/repos?owner_name=BoyCook'
+	// 'https://api.github.com/users/BoyCook/repos?per_page=100'
+	// 'https://api.github.com/users/BoyCook/repos?name=1,2,3' - ? will that work
 }
 
 DevOps.prototype.loadRepos = function() {
-	for (var i=0,len=this.accounts.length; i<len; i++) {
-		this.getRepos(this.accounts[i]);
+	for (var account in this.config.accounts) { 
+		var repos = this.config.accounts[account];
+		if (repos instanceof Array) {
+			// Get each repo
+			for (var i=0,len=repos.length; i<len; i++) {
+				this.getRepo(account, repos[i]);
+			}
+		} else if (name === '*') {
+			// Get account repos
+			this.getAccountRepos(account);
+		} 
 	}
 };
 
-DevOps.prototype.getRepos = function(name) {
+DevOps.prototype.getAccountRepos = function(account) {
 	var context = this;
-	var url = this.urls.github + '/users/' + name +  '/repos?per_page=100';
+	var url = this.urls.github + '/users/' + account +  '/repos?per_page=100';
 	var callBack = function(data) {
-		context.repos[name] = data;
-		$('.content').append(context.templates.list({ name: name, repos: data}));
+		context.accounts[account] = data;
+		$('.content').append(context.templates.list({ name: account, repos: data}));
+	};
+	$.getJSON(url, callBack);
+};
+
+DevOps.prototype.getRepo = function(account, name) {
+	var context = this;
+	var url = this.urls.github + '/users/' + account +  '/' + name;
+	var callBack = function(data) {
+		context.accounts[account].push(data);
+		//TODO - append single row to table at a time
+		// $('.content').append(context.templates.list({ name: account, repos: data}));
 	};
 	$.getJSON(url, callBack);
 };
@@ -32,6 +59,12 @@ DevOps.prototype.loadTemplates = function() {
 	this.templates = {
 		list: Handlebars.compile($("#project-list-template").html())
 	};	
+};
+
+DevOps.prototype.setupCache = function() {
+	for (var key in this.config.accounts) { 
+		this.accounts[key] = [];
+	}
 };
 
 var app = undefined;
